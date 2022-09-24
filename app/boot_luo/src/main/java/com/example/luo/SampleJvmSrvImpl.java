55d784f85a7643fe6b45fba15935b6a8c68cf733
luoxun <luoxun007@gmail.com>
2022-09-24 22:08:13 +0800

    up

--- app/boot_luo/src/main/java/com/example/luo/SampleJvmSrvImpl.java
+++ app/boot_luo/src/main/java/com/example/luo/SampleJvmSrvImpl.java
@@ -1,29 +1,33 @@
 package com.example.luo;
 
+import com.alipay.sofa.runtime.api.annotation.SofaService;
 import com.example.facade.SampleJvmSrv;
 
+// @SofaService
 public class SampleJvmSrvImpl implements SampleJvmSrv {
 
-
-    public SampleJvmSrvImpl(){}
+    public SampleJvmSrvImpl() {
+    }
 
     public String getMessage() {
         return message;
     }
 
     public void setMessage(String message) {
+        System.out.println("setMessagesetMessagesetMessagesetMessagesetMessagesetMessagesetMessagesetMessage");
         this.message = message;
     }
 
     private String message;
 
-    public SampleJvmSrvImpl(String message){
-        this.message =  message;
+    public SampleJvmSrvImpl(String message) {
+        System.out.println(
+                "11111111SampleJvmSrvImplSampleJvmSrvImplSampleJvmSrvImplSampleJvmSrvImplSampleJvmSrvImplSampleJvmSrvImpl11111");
+        this.message = message;
     }
-
 
     @Override
     public String message() {
-        return null;
+        return this.message;
     }
 }
